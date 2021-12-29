c781dbb3fbc88c8c7eb5c4f16e5877925ed59bb5
AiluropodaMicrota <panda515@protonmail.com>
2021-12-29 18:28:29 +0300

    Wrapped video actors into expandable layout

--- app/src/main/java/com/th/pv/VideoEditFragment.kt
+++ app/src/main/java/com/th/pv/VideoEditFragment.kt
@@ -2,15 +2,19 @@ package com.th.pv
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.Navigation
 import com.th.pv.data.Actor
 import com.th.pv.data.ActorVideo
 import com.th.pv.data.PVData
 import kotlin.math.round
 
-class VideoEditFragment(var pvData: PVData, var video : ActorVideo) : DialogFragment() {
+
+class VideoEditFragment(var pvData: PVData, var video: ActorVideo) : DialogFragment() {
     lateinit var dialog : AlertDialog
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let { it ->
@@ -18,11 +22,27 @@ class VideoEditFragment(var pvData: PVData, var video : ActorVideo) : DialogFrag
             val inflater = requireActivity().layoutInflater;
             val root = inflater.inflate(R.layout.actor_videos_edit, null)
 
-            val ratingBar = root.findViewById<me.zhanghai.android.materialratingbar.MaterialRatingBar>(R.id.ratingBar)
+            val ratingBar = root.findViewById<me.zhanghai.android.materialratingbar.MaterialRatingBar>(
+                R.id.ratingBar
+            )
             ratingBar.rating = video.rating.toFloat() / 2
 
+            val layoutVideoActors = root.findViewById<net.cachapa.expandablelayout.ExpandableLayout>(R.id.videoActorsLayout)
+            val iconToggleVideoActors = root.findViewById<ImageView>(R.id.iconToggleVideoActors)
+            val layoutToggleVideoActors = root.findViewById<LinearLayout>(R.id.layoutToggleVideoActors)
+            layoutToggleVideoActors.setOnClickListener {
+                if (layoutVideoActors.isExpanded) {
+                    layoutVideoActors.collapse()
+                    iconToggleVideoActors.setImageResource(R.drawable.ic_expand)
+                }
+                else {
+                    layoutVideoActors.expand()
+                    iconToggleVideoActors.setImageResource(R.drawable.ic_collapse)
+                }
+            }
+
             val videoActorSelectorAdapter = ActorSelectorAdapter(requireActivity(), pvData)
-            var videoActorSelector = root.findViewById<ListView>(R.id.videoActorsSelector)
+            val videoActorSelector = root.findViewById<ListView>(R.id.videoActorsSelector)
             videoActorSelector.adapter = videoActorSelectorAdapter
             videoActorSelector.choiceMode = ListView.CHOICE_MODE_MULTIPLE
             for (i in 0 until videoActorSelector.count){
@@ -31,7 +51,8 @@ class VideoEditFragment(var pvData: PVData, var video : ActorVideo) : DialogFrag
             }
 
             builder.setView(root)
-                .setPositiveButton("Ok"
+                .setPositiveButton(
+                    "Ok"
                 ) { dialog, id ->
                     video.rating = round(ratingBar.rating * 2).toInt()
 
@@ -45,7 +66,8 @@ class VideoEditFragment(var pvData: PVData, var video : ActorVideo) : DialogFrag
                     postVideoData(activity as MainActivity, video) {}
                     (activity as MainActivity).update()
                 }
-                .setNegativeButton("Cancel"
+                .setNegativeButton(
+                    "Cancel"
                 ) { dialog, id ->
                     dialog.cancel()
                 }
